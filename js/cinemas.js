44fe22e13247ca5c2671470f99a91e49a52de8d2
Anton <gagonin1996@mail.ru>
2022-04-16 00:56:38 +0300

    Initial commit

---
+++ js/cinemas.js
@@ -0,0 +1,48 @@
+function getCinemas() {
+    return [{
+        branchName: "City Cinema - Central",
+        movies: [{
+            id: 1,
+            shows: [{
+                index: 1,
+                datetime: "11 Nov, Wed - 9:00am",
+                house: 4
+            }]
+        }]
+    }, {
+        branchName: "City Cinema - Mong Kok",
+        movies: [{
+            id: 2,
+            shows: [{
+                index: 2,
+                datetime: "11 Nov, Wed - 9:00am",
+                house: 4
+            }, {
+                index: 3,
+                datetime: "12 Nov, Thu - 11:00am",
+                house: 2
+            }]
+        }]
+    }, {
+        branchName: "City Cinema - Festival Walk",
+        movies: [{
+            id: 1,
+            shows: [{
+                index: 4,
+                datetime: "8 Nov, Sun - 9:00am",
+                house: 3
+            }, {
+                index: 5,
+                datetime: "10 Nov, Tue - 11:00am",
+                house: 5
+            }]
+        }, {
+            id: 3,
+            shows: [{
+                index: 6,
+                datetime: "9 Nov, Mon - 2:00pm",
+                house: 1
+            }]
+        }]
+    }]
+}
---
+++ js/movies.js
@@ -0,0 +1,57 @@
+function getMovies() {
+    return [{
+        id: 1,
+        type: "now",
+        thumbnail: "casablanca.png",
+        src: "http://courses.cs.cityu.edu.hk/cs2204/video/casablanca-s.mp4",
+        name: "Cassblanca",
+        cast: "Humphrey Bogart, Ingrid Bergmen",
+        director: "Michael Curtiz",
+        duration: 130
+    }, {
+        id: 2,
+        type: "now",
+        thumbnail: "wildlife.png",
+        src: "http://courses.cs.cityu.edu.hk/cs2204/video/wildlife.mp4",
+        name: "Wildlife",
+        cast: "Matthew McConaughey and Scarlett Johansson",
+        director: "Steven Spielberg",
+        duration: 135
+    }, {
+        id: 3,
+        type: "now",
+        thumbnail: "barbecue.png",
+        src: "http://courses.cs.cityu.edu.hk/cs2204/video/barbecue.mp4",
+        name: "Barbecue",
+        cast: "Lambert Wilson and Franck Dubosc",
+        director: "Eric Lavaine",
+        duration: 98
+    }, {
+        id: 4,
+        type: "upcoming",
+        thumbnail: "nature.png",
+        src: "http://courses.cs.cityu.edu.hk/cs2204/video/nature.mp4",
+        name: "Nature",
+        cast: "Parrot, birds",
+        director: "Mother Nature",
+        duration: 100
+    }, {
+        id: 5,
+        type: "upcoming",
+        thumbnail: "chocolate.png",
+        src: "http://courses.cs.cityu.edu.hk/cs2204/video/chocolate.mp4",
+        name: "Chocolate",
+        cast: "Juliette Binoche and Johnny Depp",
+        director: "Lasse Hallström",
+        duration: 15
+    }, {
+        id: 6,
+        type: "upcoming",
+        thumbnail: "wonders.png",
+        src: "http://courses.cs.cityu.edu.hk/cs2204/video/wonders.mp4",
+        name: "Wonders",
+        cast: "Alba Rohrwacher and Maria Alexandra Lungu",
+        director: "Alice Rohrwacher",
+        duration: 110
+    }]
+}
